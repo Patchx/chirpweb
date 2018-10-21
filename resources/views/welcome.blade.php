1c4ca6884ed7b1b6aa7823427e73dffdc9d1df74
Robert Anderson <robert.anderson.fl@gmail.com>
2018-10-21 18:39:13 -0400

    Moved the flatty theme sass code into a local file on the server. Reorganzed the flatty theme sass and tweaked the spacing of the landing page banner.  Removed redundant or unnecessary external javascript

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,28 +6,31 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
-    
-    <link rel="shortcut icon" href="https://usebootstrap.com/preview/flatty/assets/img/favicon.png">
-    
+        
     <title>Chirp</title>
 
-    <link href="https://usebootstrap.com/preview/flatty/assets/css/bootstrap.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
-    <link href="https://usebootstrap.com/preview/flatty/assets/css/main.css" rel="stylesheet">
+    <link rel="stylesheet" href="/css/flatty_theme.css">
 
     <link href='http://fonts.googleapis.com/css?family=Lato:300,400,900' rel='stylesheet' type='text/css'>
 
     <!--[if lt IE 9]>
-          <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-          <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
-        <![endif]-->
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
 </head>
 
 <body>
     <div class="navbar navbar-default navbar-fixed-top">
         <div class="container">
             <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                <button 
+                    type="button" 
+                    class="navbar-toggle" 
+                    data-toggle="collapse" 
+                    data-target=".navbar-collapse"
+                >
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
@@ -38,7 +41,8 @@
     
             <div class="navbar-collapse collapse">
                 <ul class="nav navbar-nav navbar-right">
-                    <li><a href="#">Already a member?</a></li>
+                    <li><a href="#">Signup</a></li>
+                    <li><a href="#">Login</a></li>
                 </ul>
             </div>
         </div>
@@ -48,28 +52,33 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-6">
-                    <h1><strong>Chirp</strong></h1>
+                    <h1>
+                        <strong>Chirp</strong>
+                    </h1>
 
-                    <h1>Get There, on time</h1>
+                    <h2>Get There, On Time</h2>
     
-                    <form class="form-inline" role="form">
-                        <div class="form-group">
-                            <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter your email address">
+                    <form class="form-inline mt-25" role="form">
+                        <div class="form-group mr-10">
+                            <input 
+                                type="email" 
+                                class="form-control" 
+                                id="exampleInputEmail1" 
+                                placeholder="Enter your email address">
                         </div>
     
-                        <button type="submit" class="btn btn-warning btn-lg">Invite Me!</button>
+                        <button 
+                            type="submit" 
+                            class="btn btn-warning btn-lg"
+                        >Invite Me!</button>
                     </form>
-                </div>
-    
-                <div class="col-lg-6">
-                    <img class="img-responsive" src="https://usebootstrap.com/preview/flatty/assets/img/ipad-hand.png" alt="">
                 </div>
             </div>
         </div>
     </div>
     
     <div class="container">
-        <div class="row mt centered">
+        <div class="row vert-spacing centered">
             <div class="col-lg-6 col-lg-offset-3">
                 <h1>Your Landing Page<br />Looks Wonderful Now.</h1>
                 
@@ -77,24 +86,18 @@
             </div>
         </div>
     
-        <div class="row mt centered">
-            <div class="col-lg-4">
-                <img src="https://usebootstrap.com/preview/flatty/assets/img/ser01.png" width="180" alt="">
-                
+        <div class="row vert-spacing centered">
+            <div class="col-lg-4">                
                 <h4>1 - Browser Compatibility</h4>
                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever.</p>
             </div>
             
-            <div class="col-lg-4">
-                <img src="https://usebootstrap.com/preview/flatty/assets/img/ser02.png" width="180" alt="">
-                
+            <div class="col-lg-4">                
                 <h4>2 - Email Campaigns</h4>
                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever.</p>
             </div>
     
-            <div class="col-lg-4">
-                <img src="https://usebootstrap.com/preview/flatty/assets/img/ser03.png" width="180" alt="">
-    
+            <div class="col-lg-4">    
                 <h4>3 - Gather Your Notes</h4>
                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever.</p>
             </div>
@@ -102,25 +105,29 @@
     </div>
     
     <div class="container">
-        <div class="row mt centered">
+        <div class="row vert-spacing centered">
             <hr>
 
             <div class="col-lg-6 col-lg-offset-3">
                 <h1>Stay Updated!</h1>
                 <h3>Be the first to hear about new features</h3>
+                <br>
             </div>
-        </div>
-    </div>
 
-    <div class="container">        
-        <div class="row centered">
             <div class="col-lg-6 col-lg-offset-3">
                 <form class="form-inline" role="form">
-                    <div class="form-group">
-                        <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter your email address">
+                    <div class="form-group mr-15">
+                        <input 
+                            type="email" 
+                            class="form-control" 
+                            id="exampleInputEmail1" 
+                            placeholder="Enter your email address">
                     </div>
     
-                    <button type="submit" class="btn btn-warning btn-lg">Invite Me!</button>
+                    <button 
+                        type="submit" 
+                        class="btn btn-warning btn-lg"
+                    >Invite Me!</button>
                 </form>
             </div>
     
@@ -133,9 +140,5 @@
     
         <p class="centered">Design Modified from BlackTie.co - Attribution License 3.0 - 2013</p>
     </div>
-
-    <script src="https://code.jquery.com/jquery-1.10.2.min.js" type="423aaaa3dc0a9203b1cd2c0a-text/javascript"></script>
-    <script src="https://usebootstrap.com/preview/flatty/assets/js/bootstrap.min.js" type="423aaaa3dc0a9203b1cd2c0a-text/javascript"></script>
-    <script src="https://ajax.cloudflare.com/cdn-cgi/scripts/2448a7bd/cloudflare-static/rocket-loader.min.js" data-cf-nonce="423aaaa3dc0a9203b1cd2c0a-" defer=""></script>
 </body>
 </html>
