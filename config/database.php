2cec2a53f02fea005dff986d9a4a02ee0ee00c93
Robert Anderson <robert.anderson.fl@gmail.com>
2018-10-29 21:09:14 -0400

    *env update required - Added a AWS RDS database connection to the database config file

--- config/database.php
+++ config/database.php
@@ -54,6 +54,21 @@ return [
             'engine' => null,
         ],
 
+        'rds' => [
+            'driver' => 'mysql',
+            'host' => env('CLOUD_DB_HOST', '127.0.0.1'),
+            'port' => env('CLOUD_DB_PORT', '3306'),
+            'database' => env('CLOUD_DB_DATABASE', 'forge'),
+            'username' => env('CLOUD_DB_USERNAME', 'forge'),
+            'password' => env('CLOUD_DB_PASSWORD', ''),
+            // 'unix_socket' => env('CLOUD_DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'strict' => true,
+            'engine' => null,
+        ],
+
         'pgsql' => [
             'driver' => 'pgsql',
             'host' => env('DB_HOST', '127.0.0.1'),
